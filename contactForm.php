fc2053942d44ceaa91395620e9bc7ecf8a089fd4
An Vu <70239307+avunguye@users.noreply.github.com>
2024-04-07 17:40:48 -0500

    Update contactForm.php

--- contactForm.php
+++ contactForm.php
@@ -1,1 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['message'];
+  $to = 'avunguye@gmail.com'; 
+  $subject = 'Contact Submission from Portfolio Website';
+  $body = "Name: $name\nEmail: $email\nMessage: $message";
 
+if (mail($to, $subject, $body)) {
+  echo "Thank you for your submission!";
+} else {
+  echo "Error sending email. Please try again later.";
+}
+}
+?>
